75f4f06f53a45c7cf1663d29fca3166d307ea581
Gioele <gioele.dellaceca@gmail.com>
2021-11-15 23:58:53 +0100

    Changed prime number algorithm

--- primenmutit.py
+++ primenmutit.py
@@ -1,10 +1,10 @@
 """ An effort to understand parallel programming.
 """
 import time
-
+import numpy as np
 
 def primenumbers(n):
-    '''This takes a number and outputs a list of all primes before it, order n**2, slow'''
+    '''This takes a number and outputs a list of all primes before it, order at least n**2, slow'''
     primes = [2]
     if n <= 2:
         return []
@@ -20,18 +20,34 @@ def primenumbers(n):
     return primes
 
 
-def summingSeq(numbers):
+def sieveErat(n):
+    """A better algorithm to find prime numbers, by eratosthenes, using numpy for speed"""
+    if n < 2:
+        return 0
+
+    numbers = np.ones(n+1, dtype=np.bool_)
+    p = 2
+    while (p * p <= n):
+        if (numbers[p] == np.True_):
+            for i in range(p**2, n+1 , p):
+                numbers[i] = np.False_
+        p += 1
+    numbers[0] = np.False_
+    numbers[1] = np.False_
+    return numbers.nonzero()
+
+def summingSeq(n):
 
     listsum = []
-    for i in numbers:
-        listsum.append([i, sum(primenumbers(i))])
+    for i in n:
+        listsum.append([i, np.sum(sieveErat(i))])
 
     return listsum
 
     
 if __name__ == '__main__':
 
-    numlist = range(1000)
+    numlist = range(2, 10)
     
     startseq = time.perf_counter()
     pnumber = summingSeq(numlist)
